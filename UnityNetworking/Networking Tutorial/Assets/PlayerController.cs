b7c9e565e2d1e1c481e40e502cca3ea8b1a1609c
TerrenceChan <roxes2112@yahoo.com>
2018-01-26 16:03:16 +0800

    *Initial files from Unity networking tutorial.
    *Most recent tutorial is Episode 2.

---
+++ UnityNetworking/Networking Tutorial/Assets/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+// PlayerController can be controlled by a player.
+
+public class PlayerController : NetworkBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		// This function runs for all units, NOT just my own.
+
+		// How to verify only I can mess around with my GameObject?
+
+		if (!hasAuthority) {
+			return;
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			this.transform.Translate (0.0f, 1.0f, 0.0f);
+		}
+
+		if (Input.GetKeyDown (KeyCode.D)) {
+			Destroy (this.gameObject);
+		}
+	}
+}
